4bd44d727327d63b8a5f820aa5136a1a42955c39
Chris Lloyd <chris.lloyd.hi@gmail.com>
2016-07-14 09:58:30 +1200

    Oops - indentation errors

--- responses/BaseResponse.py
+++ responses/BaseResponse.py
@@ -26,11 +26,11 @@ class ResponseBase:
     		"text": message
 		}))	
 
-    # Checks to see if a string (e.g. incoming message) has words contained in an array
-    # string - the message string
-    # keywords - an array of keywords (strings) ['hi','hello] for example
-    def keyword_match(self, string, keywords):
-        for word in string.split():
-            if word in keywords:
-                return True
-        return False
+	# Checks to see if a string (e.g. incoming message) has words contained in an array
+	# string - the message string
+	# keywords - an array of keywords (strings) ['hi','hello] for example
+	def keyword_match(self, string, keywords):
+		for word in string.split():
+			if word in keywords:
+				return True
+		return False
